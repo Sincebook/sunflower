e5f428d8fa2135bb6596bc3956e1544a078d9586
since <1336594801@qq.com>
2022-01-27 15:30:36 +0800

    修改运行端口

--- ci/deploy_dev01.sh
+++ ci/deploy_dev01.sh
@@ -4,4 +4,4 @@ tar zxvf ./../frontend.tar.gz
 cd ..
 docker build -f ./ci/Dockerfile -t $IMAGE_REPO_PREFIX-dev01:$IMAGE_VER .
 docker stop $CONTAINER_NAME_PREFIX-dev01 || true && docker rm $CONTAINER_NAME_PREFIX-dev01 || true
-docker run -d -p 127.0.0.1:18890:80 --name $CONTAINER_NAME_PREFIX-dev01 $IMAGE_REPO_PREFIX-dev01:$IMAGE_VER
+docker run -d -p 127.0.0.1:18188:80 --name $CONTAINER_NAME_PREFIX-dev01 $IMAGE_REPO_PREFIX-dev01:$IMAGE_VER
