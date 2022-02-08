94102990e5ea598904df0d6ffb6d3471f2f300d1
zzz <1760189000@qq.com>
2022-02-08 15:24:20 +0800

    修改了导航栏

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -5,59 +5,65 @@
  */
 loader.define(function(require, exports, module) {
 
-    var pageview = {
-        init: function() {
-            this.tab();
-        },
-        tab: function() {
+    var pageview = {},
+        uiDialogNav;
 
-            var uiTab = bui.tab({
-                id: "#uiTab",
-                position: "bottom",
-                iconPosition: "top",
-                data: [{
-                        id: "tab0",
-                        icon: "icon-menu",
-                        title: "课程",
-                        name: "pages/lesson/lesson",
-                        param: { type: "news" }
-                    }, {
-                        id: "tab1",
-                        icon: "icon-menu",
-                        title: "任务",
-                        name: "pages/task/task",
-                        param: { type: "news" }
-                    }, {
-                        id: "tab2",
-                        icon: "icon-home",
-                        title: "主页",
-                        name: "pages/home/home",
-                        param: { type: "photo" },
-                        everytime: true
-                    },
-                    {
-                        id: "tab3",
-                        icon: "icon-pic",
-                        title: "培训",
-                        name: "pages/train/train",
-                        param: { type: "video" }
-                    }, {
-                        id: "tab4",
-                        icon: "icon-user",
-                        title: "我的",
-                        name: "pages/mine/mine",
-                        param: { type: "class" }
-                    }
-                ]
+    // 模块初始化定义
+    pageview.init = function() {
+        navTab();
+
+        // 栏目弹出菜单, 因为要遮住底部的导航, 所以弹出层需要跟底部导航在一块初始化
+        uiDialogNav = bui.dialog({
+            id: "#uiDialog",
+            position: "right",
+            fullscreen: true,
+            effect: "fadeInRight",
+            mask: false
+        });
+    }
+
+    // 底部导航
+    function navTab() {
+
+        //menu在tab外层,menu需要传id
+        var tab = bui.tab({
+                id: "#tabDynamic",
+                menu: "#tabDynamicNav",
+                swipe: false,
+                animate: false,
+                // 1: 声明是动态加载的tab
+                autoload: true,
             })
-        }
-    };
-
+            // 2: 监听加载后的事件, load 只加载一次
+        tab.on("to", function(index) {
+            var current = index || 0;
+            switch (current) {
+                case 0:
+                    loader.require(["pages//"])
+                    break;
+                case 1:
+                    loader.require(["pages/task/task"])
+                    break;
+                case 2:
+                    loader.require(["pages/home/home"])
+                    break;
+                case 3:
+                    loader.require(["pages/train/train"])
+                    break;
+                case 4:
+                    loader.require(["pages/mine/mine"])
+                    break;
+            }
+        }).to(0)
+    }
 
     // 初始化
     pageview.init();
 
+    // 把弹出层模块抛出去
+    pageview.columnDialog = uiDialogNav;
+
     // 输出模块
-    return pageview;
+    module.exports = pageview;
 
 })
