499b76263bdb94e9d7559a34e1aac46f6ad60bb5
zzz <1760189000@qq.com>
2022-02-09 15:47:50 +0800

    修改了train页面结构，调整了mine和task页面的小问题

--- src/pages/train/train.js
+++ src/pages/train/train.js
@@ -1,9 +1,29 @@
 loader.define(function() {
+    var pageview = {
+        init: function() {
+            var uiTabHead = bui.tab({
+                id: "#uiTabHead",
+                position: "top",
+                iconPosition: "left",
+                data: [{
+                    id: "uiTabHead0",
+                    title: "未报名",
+                    name: "pages/train_page1/train_page1",
+                    param: { type: "news" }
+                }, {
+                    id: "uiTabHead1",
+                    title: "已报名",
+                    name: "pages/train_page2/train_page2",
+                    param: { type: "photo" },
+                }, ]
+            })
 
-    //按钮在tab外层,需要传id
-    var tab = bui.tab({
-        id: "#tabHead",
-        menu: "#tabHeadNav",
-    })
+            // uiTabHead.addBadge(1, 5)
+            // uiTabHead.addBadge(1, 3)
+            // uiTabHead.removeBadge()
 
+        }
+    }
+    pageview.init();
+    return pageview;
 })
