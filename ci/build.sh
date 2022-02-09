32dd32be4ec27b8b6c3d7d64f3fcaee0736eccb0
since <1336594801@qq.com>
2022-02-09 23:07:30 +0800

    修改打包方式

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,8 @@
 # This script should build frontend and pack it as "frontend.tar.gz" in the working directory.
 # Working directory is guaranteed to be the root of the frontend project where package.json exists.
 
-yarn install
-yarn build
+npm i
+npm run build
 
 cd dist
 tar zcvf ./../frontend.tar.gz .
