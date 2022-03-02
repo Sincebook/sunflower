5d36bb79bea21335f80b8ac9311c5d36fd384c5f
since <1336594801@qq.com>
2022-03-03 00:15:29 +0800

    feat: 考试题目显示

--- src/pages/exam/exam.js
+++ src/pages/exam/exam.js
@@ -2,15 +2,21 @@ loader.define(function () {
     var bs = bui.store({
         scope: "page", // 用于区分公共数据及当前数据的唯一值
         data: {
-            choices: [],
-            nums:1
+            choices: {
+                title: 1,
+                answer: '',
+                image: '',
+                knowledge: '',
+            },
+            choice: [],
+            nums: 0
         },
         // log: true,
         methods: {
-           
+
         },
         watch: {
-            
+
         },
         computed: {},
         templates: {},
@@ -19,14 +25,20 @@ loader.define(function () {
             getExam({
                 lesson_id: params.id
             }).then(res => {
-                this.choices = res.data
+                let datas = res.data
+                
+                this.choice = eval(datas[this.nums].choice)
+                this.choices = datas[this.nums]
+                this.choices.da
+                console.log(this.choices, this.choice)
+
             })
         }
     })
     var pageview = {};
     pageview.init = function () {
-        
-        
+
+
     }
     pageview.init()
     return pageview
