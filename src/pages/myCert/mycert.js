1254d3c28d3352210598dce71036d2987696148f
since <1336594801@qq.com>
2022-03-04 23:03:12 +0800

    feat: 证书完成

--- src/pages/myCert/mycert.js
+++ src/pages/myCert/mycert.js
@@ -1,5 +1,67 @@
 loader.define(function () {
     findMyCert({lesson_id:1}).then(res => {
+        if (res.code == '0') {
+            let html = ''
+            res.data.forEach(element => {
+                let td = ''
+                element.finishMediaDtoList.forEach(e => {
+                    td+=` <tr>
+                    <td>${e.name}</td>
+                    <td>${element.classesName}</td>
+                    <td>${e.type == '1'? '普通课程':'优质课程'}</td>
+                    </tr>`
+                })
+                let imgStudy = ''
+                let timeStudy = ''
+                element.studyImg.forEach(e => {
+                    imgStudy+=`<td><img src="${e.image}"></td>`
+                    timeStudy+=`<td>${getTime(e.uptime)}</td>`
+                })
+                html +=`<br>
+                <div class="bui-box-space">
+			<div class="span1">
+				<div class="bui-sub primary" data-sub="合格" style="width: 100%;background: #fff;padding:0 40px;color: #000;">
+                    <div style="text-align:center;line-height: 60px;">
+                        <h3 stlye="">道路运输从业人员安全教育证明</h3>
+                    </div>
+                    <div style="font-size: 8px;">
+                    姓名：${element.usersName}<br>
+                    身份证号：${element.usersIdcard}<br>
+                    单位名称：${element.usersName}<br>
+                    培训内容：${element.usersCompanyName}<br>
+                    培训类型：${element.classesName}<br>
+                    考试成绩：${element.score}<br><br>
+                    学习内容:
+                    <table border="1" style="text-align: center;">
+                        <tr>
+                        <th>课程</th>
+                        <th>类别</th>
+                        <th>课程类型</th>
+                        </tr>
+                       ${td}
+                    </table>
+                    <br>
+                    <table border="1" style="text-align: center;">
+                            <tr>${imgStudy}
+                            </tr>
+                            <tr>${timeStudy}
+                            </tr>
+                    </table>
+                    <br>
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp该同志已完成道路运输从业人员安全教育培训学习内容，学习期满，成绩合格。
+                    <br>
+                    </div>
+                    <br> 
+                    <div style="float: right;font-size: 8px;">
+                        <img src="../../images/2022030417510515.gif" alt="" style="float: right;width: 50%;"><br><br><br><br>
+                        <span style="float: right;">2020年3月12日</span><br><br>
+                    </div>
+				</div>
+			</div>
+		</div>`
+            });
+            document.getElementById('res').innerHTML = html
+        }
         console.log(res)
     })
     var pageview = {};
