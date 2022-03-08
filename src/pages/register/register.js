ed151df0c54a8f24f4415c6980063790c08910d6
since <1336594801@qq.com>
2022-03-08 10:37:14 +0800

    增加提示

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -66,6 +66,8 @@ loader.define(function () {
                                         }
                                     });
                                 });
+                            } else {
+                                bui.alert(res.errMsg);
                             }
                         })
                     }
